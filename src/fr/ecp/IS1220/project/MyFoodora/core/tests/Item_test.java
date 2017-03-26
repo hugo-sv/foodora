8359293101c591b0db426d53120f079938b8514f
Rémi Calixte <remi.calixte@student.ecp.fr>
2017-03-26 19:11:40 +0200

    Item Test

--- src/fr/ecp/IS1220/project/MyFoodora/core/tests/Item_test.java
+++ src/fr/ecp/IS1220/project/MyFoodora/core/tests/Item_test.java
@@ -4,11 +4,13 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-public class Item_test {
+import fr.ecp.IS1220.project.MyFoodora.core.Dessert;
 
+public class Item_test {
+	static Dessert dessert = new Dessert("Tarte à la rubarbe", 23, true, false);
 	@Test
-	public void testIsStandard() {
-		fail("Not yet implemented");
+	public void testIsVegetarian() {
+		assertTrue(dessert.isVegetarian());
 	}
 
 }

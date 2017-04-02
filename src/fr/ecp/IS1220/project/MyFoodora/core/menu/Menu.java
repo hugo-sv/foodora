4699a318af8afadb66e539cf6ac89cdcdbb8cf24
Hugo Saint-Vignes <hugo.saint-vignes@student.ecp.fr>
2017-04-02 13:34:42 +0100

    Ajout des fonctions removeItem et removeMeal

--- src/fr/ecp/IS1220/project/MyFoodora/core/menu/Menu.java
+++ src/fr/ecp/IS1220/project/MyFoodora/core/menu/Menu.java
@@ -4,18 +4,41 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Menu {
-	// Set lists don't allow doubles
+	// Set lists avoid doubles
 	Set<Item> items = new HashSet<Item>();
 	Set<Meal> meals = new HashSet<Meal>();
 
 	public void addItem(Item item) {
 		items.add(item);
 	}
+
 	public void addMeal(Meal meal) {
-		
-		for(Item item:meal.getItems()){
-			items.add(item);}
+		// Add Meal's items to the item list
+		for (Item item : meal.getItems()) {
+			items.add(item);
+		}
 		meals.add(meal);
+	}
+
+	public void removeItem(Item item) {
+		// Remove the item from the items set
+		for (Meal meal : meals) {
+			if (meal.getItems().contains(item)) {
+				// The removed Item is used in a meal
+				this.removeMeal(meal);
+			}
+		}
+		if (items.contains(item)) {
+			items.remove(item);
+		}
+	}
+
+	public void removeMeal(Meal meal) {
+		// Remove the meal from the meal set
+		if (meals.contains(meal)) {
+			meals.remove(meal);
+		}
+		// Meal's item are not removed
 	}
 
 }

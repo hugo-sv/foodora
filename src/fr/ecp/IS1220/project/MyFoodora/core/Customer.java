319674ecde698ec8bb361bcc0a47e3666a80e408
Hugo Saint-Vignes <hugo.saint-vignes@student.ecp.fr>
2017-04-05 00:27:58 +0200

    Ajout d'attributes sur user

--- src/fr/ecp/IS1220/project/MyFoodora/core/Customer.java
+++ src/fr/ecp/IS1220/project/MyFoodora/core/Customer.java
@@ -5,23 +5,26 @@ import java.util.ArrayList;
 import fr.ecp.IS1220.project.MyFoodora.core.menu.Visitable;
 
 public class Customer implements User{
-	//A compléter
+	//have a name, a surname, a unique ID, an address (expressed as a two
+	//dimensional co-ordinate), an email address, a phone number and a username, the
+	//latter used for logging in the system.
 	private ArrayList<Visitable> shoppingCart = new ArrayList<Visitable>();
 	private double shoppingCartPrice = 0;
-	static long genID = 0;
-	long ID;
-	String Name;
-	String Surname;
-	String Username;
+	private static long genID = 0;
+	private long iD;
+	private String name;
+	private String surname;
+	private String username;
+	private boolean fidelityCard=false;
 	
 	
 	public Customer(long iD, String name, String surname, String username) {
 		super();
-		ID = genID;
+		this.iD = genID;
 		genID++;
-		Name = name;
-		Surname = surname;
-		Username = username;
+		this.name = name;
+		this.surname = surname;
+		this.username = username;
 	}
 	
 	public void visit(Visitable v){

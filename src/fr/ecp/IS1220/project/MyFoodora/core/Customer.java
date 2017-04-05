e5cd355f448d5d750068136c9c5607d0701388af
Hugo Saint-Vignes <hugo.saint-vignes@student.ecp.fr>
2017-04-05 15:40:08 +0200

    Ajout mineurs + tests

--- src/fr/ecp/IS1220/project/MyFoodora/core/Customer.java
+++ src/fr/ecp/IS1220/project/MyFoodora/core/Customer.java
@@ -4,41 +4,51 @@ import java.util.ArrayList;
 
 import fr.ecp.IS1220.project.MyFoodora.core.menu.Visitable;
 
-public class Customer implements User{
-	//have a name, a surname, a unique ID, an address (expressed as a two
-	//dimensional co-ordinate), an email address, a phone number and a username, the
-	//latter used for logging in the system.
+public class Customer implements User {
+	// have a name, a surname, a unique ID, an address (expressed as a two
+	// dimensional co-ordinate), an email address, a phone number and a
+	// username, the
+	// latter used for logging in the system.
 	private ArrayList<Visitable> shoppingCart = new ArrayList<Visitable>();
-	private double shoppingCartPrice = 0;
 	private static long genID = 0;
 	private long iD;
 	private String name;
 	private String surname;
 	private String username;
 	private ArrayList<Double> address;
-	private String eamil;
+	private String email;
 	private String phoneNumber;
-	private boolean fidelityCard=false;
-	
-	
-	public Customer(long iD, String name, String surname, String username) {
+	private boolean fidelityCard = false;
+
+	public Customer(String name, String surname, String username, ArrayList<Double> address, String email,
+			String phoneNumber) {
 		super();
 		this.iD = genID;
 		genID++;
 		this.name = name;
 		this.surname = surname;
 		this.username = username;
+		this.address = address;
+		this.email = email;
+		this.phoneNumber = phoneNumber;
 	}
-	
-	public void visit(Visitable v){
+
+	public ArrayList<Double> getAddress() {
+		return address;
+	}
+
+	public void setAddress(ArrayList<Double> address) {
+		this.address = address;
+	}
+
+	public void visit(Visitable v) {
 		shoppingCart.add(v);
-		shoppingCartPrice=shoppingCartPrice+v.getPrice();
-		}
-	
-	public  double getPrice(){
-		double Price =0;
-		for (Visitable v:shoppingCart){
-			Price=Price+v.getPrice();
+	}
+
+	public double getPrice() {
+		double Price = 0;
+		for (Visitable v : shoppingCart) {
+			Price = Price + v.getPrice();
 		}
 		return Price;
 	}
@@ -87,10 +97,4 @@ public class Customer implements User{
 		return shoppingCart;
 	}
 
-	public double getShoppingCartPrice() {
-		return shoppingCartPrice;
-	}
-	
-	
-	
 }
---
+++ src/fr/ecp/IS1220/project/MyFoodora/core/MéhineMocheAsupprimerSiInutile.java
@@ -0,0 +1,26 @@
+package fr.ecp.IS1220.project.MyFoodora.core;
+
+import java.util.ArrayList;
+
+import fr.ecp.IS1220.project.MyFoodora.core.menu.Dessert;
+
+public class MéhineMocheAsupprimerSiInutile {
+	public static void main(String[] args) {
+		ArrayList<Double> ChezMichel = new ArrayList<Double>();
+		ChezMichel.add((double) 15);
+		ChezMichel.add((double) 15);
+		Customer michel = new Customer("Michel", "Delatour", "Michou", ChezMichel, "michou@michou.fr", "0678910111");
+		Customer jeanne = new Customer("Jeanne", "D'arc", "Jaja", ChezMichel, "jaja@aufeu.fr", "0666666666");
+		System.out.println(michel.getiD());
+		System.out.println(michel.toString());
+		System.out.println(michel.getPrice());
+		System.out.println(michel.getAddress());
+		System.out.println(jeanne.getiD());
+		System.out.println(jeanne.toString());
+		System.out.println(jeanne.getPrice());
+		Dessert chocobon = new Dessert("Chocobon", 15, true, false);
+		jeanne.visit(chocobon);
+		System.out.println(jeanne.getPrice());
+
+	}
+}

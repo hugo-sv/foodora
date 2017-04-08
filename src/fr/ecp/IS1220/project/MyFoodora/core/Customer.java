ab4fd03786df895c1d5cc951eb2018553368bf77
Rémi Calixte <remi.calixte@student.ecp.fr>
2017-04-08 10:53:56 +0200

    Same

--- src/fr/ecp/IS1220/project/MyFoodora/core/Customer.java
+++ src/fr/ecp/IS1220/project/MyFoodora/core/Customer.java
@@ -79,4 +79,22 @@ public class Customer extends User {
 		this.fidelityCard = fidelityCard;
 	}
 
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	
+	
+
 }

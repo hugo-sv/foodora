5699d127dfcb1a3db674485051173756e2bb5639
Rémi Calixte <remi.calixte@student.ecp.fr>
2017-04-09 22:20:32 +0200

    ItemTest

--- src/fr/ecp/IS1220/project/MyFoodora/core/tests/ItemTest.java
+++ src/fr/ecp/IS1220/project/MyFoodora/core/tests/ItemTest.java
@@ -8,9 +8,42 @@ import fr.ecp.IS1220.project.MyFoodora.core.menu.Dessert;
 
 public class ItemTest {
 	static Dessert dessert = new Dessert("Tarte à la rubarbe", 23, true, false);
+
+	@Test
+	public void testGetName() {
+		assertEquals("Tarte à la rubarbe", dessert.getName());
+	}
+
+	@Test
+	public void testSetName() {
+		dessert.setName("Tarte pas bonne");
+		assertEquals("Tarte pas bonne", dessert.getName());
+	}
+
+	@Test
+	public void testGetPrice() {
+		assertTrue(dessert.getPrice() == 23);
+	}
+
+	@Test
+	public void testSetPrice() {
+		dessert.setPrice(58);
+		assertTrue(dessert.getPrice() == 58);
+	}
+
 	@Test
 	public void testIsVegetarian() {
 		assertTrue(dessert.isVegetarian());
+	}
+
+	@Test
+	public void testIsStandard() {
+		assertFalse(dessert.isStandard());
+	}
+
+	@Test
+	public void testIsGlutenFree() {
+		assertFalse(dessert.isGlutenFree());
 	}
 
 }

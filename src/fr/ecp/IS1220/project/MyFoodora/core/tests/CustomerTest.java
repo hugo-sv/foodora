f2dc8432926551c19b37725d8e51709fc1da7b77
Rémi Calixte <remi.calixte@student.ecp.fr>
2017-04-10 22:44:43 +0200

    Customer Test

--- src/fr/ecp/IS1220/project/MyFoodora/core/tests/CustomerTest.java
+++ src/fr/ecp/IS1220/project/MyFoodora/core/tests/CustomerTest.java
@@ -2,73 +2,78 @@ package fr.ecp.IS1220.project.MyFoodora.core.tests;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
-import fr.ecp.IS1220.project.MyFoodora.core.Courier;
+
 import fr.ecp.IS1220.project.MyFoodora.core.Customer;
 import fr.ecp.IS1220.project.MyFoodora.core.MyFoodora;
+import fr.ecp.IS1220.project.MyFoodora.core.cards.BasicFidelityCard;
+import fr.ecp.IS1220.project.MyFoodora.core.cards.LotteryFidelityCard;
 
 public class CustomerTest {
 	static MyFoodora foodora = new MyFoodora(0.1, 0.1, 0.1);
 	static Customer Michel = new Customer("Michel", "Michou", "michmich", 0, 10, "michel@laposte.net", "0612345678");
-
-	@Test
-	public void testGetAccountInfo() {
-		assertEquals("Name : Michel\nSurname : Michou\nEmail : michel@laposte.net\nPhone number : 0612345678\nFidelity card : Basic fidelity card\nOrders :",Michel.getAccountInfo());
+	
+	@Before
+	public void testCustomer() {
+		foodora.addUser(Michel);
 	}
 
 	@Test
 	public void testNotifyString() {
-		fail("Not yet implemented");
+		Michel.notify("This is a test");
 	}
 
 	@Test
 	public void testRegister() {
-		fail("Not yet implemented");
+		Michel.register();
+		assertTrue(foodora.getObservers().contains(Michel));
 	}
 
 	@Test
 	public void testUnregister() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testPayOrder() {
-		fail("Not yet implemented");
+		Michel.unregister();
+		assertFalse(foodora.getObservers().contains(Michel));
 	}
 
 	@Test
 	public void testGetSurname() {
-		fail("Not yet implemented");
+		assertEquals("Michou", Michel.getSurname());
 	}
 
 	@Test
 	public void testSetSurname() {
-		fail("Not yet implemented");
+		Michel.setSurname("Mich");
+		assertEquals("Mich", Michel.getSurname());
+
 	}
 
 	@Test
 	public void testGetOrders() {
-		fail("Not yet implemented");
+		assertTrue(Michel.getOrders().isEmpty());
 	}
 
 	@Test
 	public void testGetFidelityCard() {
-		fail("Not yet implemented");
+		assertTrue(Michel.getFidelityCard()instanceof BasicFidelityCard);
 	}
 
 	@Test
 	public void testSetFidelityCard() {
-		fail("Not yet implemented");
+		Michel.setFidelityCard(new LotteryFidelityCard());
+		assertTrue(Michel.getFidelityCard()instanceof LotteryFidelityCard);
+
 	}
 
 	@Test
 	public void testGetNotifyMean() {
-		fail("Not yet implemented");
+		assertEquals(Michel.getEmail(), Michel.getNotifyMean());
 	}
 
 	@Test
 	public void testSetNotifyMean() {
-		fail("Not yet implemented");
+		Michel.setNotifyMean(Michel.getPhoneNumber());
+		assertEquals(Michel.getPhoneNumber(), Michel.getNotifyMean());
 	}
 }

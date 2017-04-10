fe1eba91acd100cb3fb19963bcfb59f5a3254de4
Hugo Saint-Vignes <hugo.saint-vignes@student.ecp.fr>
2017-04-10 18:23:49 +0200

    Time test

--- src/fr/ecp/IS1220/project/MyFoodora/core/Time.java
+++ src/fr/ecp/IS1220/project/MyFoodora/core/Time.java
@@ -1,32 +1,37 @@
 package fr.ecp.IS1220.project.MyFoodora.core;
 
 public class Time {
-	private static Time currentTime=new Time(new Long(0));
+	private static Time currentTime = new Time(new Long(0));
 	private Long seconds;
-	private Time(Long seconds){
-		this.seconds=seconds;
+
+	private Time(Long seconds) {
+		this.seconds = seconds;
 	}
-	static public Time getTime(){
+
+	static public Time getTime() {
 		return new Time(currentTime.getSeconds());
 	}
-	public Long getSeconds(){
+
+	public Long getSeconds() {
 		return this.seconds;
 	}
-	static public void addTime(Long seconds){
-		currentTime.setSeconds(seconds+currentTime.getSeconds());
+
+	static public void addTime(Long seconds) {
+		currentTime.setSeconds(seconds + currentTime.getSeconds());
 	}
+
 	private void setSeconds(Long seconds) {
-		this.seconds=seconds;
+		this.seconds = seconds;
 	}
-	//Je ne comprends pas cette fonction ^^
-	public int compareTo(Time time) {
-		return this.getSeconds().compareTo(time.getSeconds());
+
+	public long compareTo(Time time) {
+		return this.getSeconds()-time.getSeconds();
 	}
-	
+
 	public boolean isLessThanOneDay() {
-		return currentTime.getSeconds() - seconds < 2592000/30;
+		return currentTime.getSeconds() - seconds < 2592000 / 30;
 	}
-	
+
 	public boolean isLessThanOneMonth() {
 		return currentTime.getSeconds() - seconds < 2592000;
 	}
--- src/fr/ecp/IS1220/project/MyFoodora/core/tests/TimeTest.java
+++ src/fr/ecp/IS1220/project/MyFoodora/core/tests/TimeTest.java
@@ -4,36 +4,61 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import fr.ecp.IS1220.project.MyFoodora.core.Time;
+
 public class TimeTest {
 
 	@Test
 	public void testGetTime() {
-		fail("Not yet implemented");
+		Time t0 = Time.getTime();
+		assertEquals(Time.getTime().getSeconds(),t0.getSeconds(), 0.0001);
 	}
 
 	@Test
 	public void testGetSeconds() {
-		fail("Not yet implemented");
+		Time t1 = Time.getTime();
+		Time.addTime((long) 20);
+		Time t2 = Time.getTime();
+		Time.addTime((long) 100);
+		assertTrue(t1.getSeconds() + 20 == t2.getSeconds());
 	}
 
 	@Test
 	public void testAddTime() {
-		fail("Not yet implemented");
+		Time t1 = Time.getTime();
+		Time.addTime((long) 70);
+		Time t2 = Time.getTime();
+		Time.addTime((long) 100);
+		assertTrue(t1.getSeconds() + 70 == t2.getSeconds());
 	}
 
 	@Test
 	public void testCompareTo() {
-		fail("Not yet implemented");
+		Time t3 = Time.getTime();
+		Time.addTime((long) 7000);
+		Time t4 = Time.getTime();
+		Time.addTime((long) 1000);
+		assertTrue(t3.compareTo(t4) == -7000);
 	}
 
 	@Test
 	public void testIsLessThanOneDay() {
-		fail("Not yet implemented");
+		Time t5 = Time.getTime();
+		Time.addTime((long) 7000);
+		assertTrue(t5.isLessThanOneDay());
+		Time.addTime((long) 86400);
+		assertTrue(!t5.isLessThanOneDay());
 	}
 
 	@Test
 	public void testIsLessThanOneMonth() {
-		fail("Not yet implemented");
+		Time t6 = Time.getTime();
+		Time.addTime((long) 7000);
+		assertTrue(t6.isLessThanOneMonth());
+		Time.addTime((long) 86400);
+		assertTrue(t6.isLessThanOneMonth());
+		Time.addTime((long) 86400*31);
+		assertTrue(!t6.isLessThanOneMonth());
 	}
 
 }

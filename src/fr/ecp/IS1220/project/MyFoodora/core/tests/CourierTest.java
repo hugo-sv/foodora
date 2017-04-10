3ad9c5078caea174a902b7c6903c74ed9d0166fe
Hugo Saint-Vignes <hugo.saint-vignes@student.ecp.fr>
2017-04-10 16:51:06 +0200

    Courrier Test (debut)

--- src/fr/ecp/IS1220/project/MyFoodora/core/tests/CourierTest.java
+++ src/fr/ecp/IS1220/project/MyFoodora/core/tests/CourierTest.java
@@ -2,6 +2,7 @@ package fr.ecp.IS1220.project.MyFoodora.core.tests;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import fr.ecp.IS1220.project.MyFoodora.core.Courier;
@@ -10,44 +11,38 @@ import fr.ecp.IS1220.project.MyFoodora.core.MyFoodora;
 public class CourierTest {
 	static MyFoodora foodora = new MyFoodora(0.1, 0.1, 0.1);
 	static Courier Fred = new Courier("Fred", "fredo", "fred", 0, 10, "Fred@laposte.net", "0612345678");
-	@Test
-	public void testCourierStringStringStringDoubleDoubleStringString() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testCourierStringStringStringDoubleDouble() {
-		fail("Not yet implemented");
-	}
 
 	@Test
 	public void testRegister() {
-		fail("Not yet implemented");
+		Fred.register(foodora);
+		assertTrue(foodora.getUserList().containsValue(Fred));
 	}
 
 	@Test
 	public void testUnregister() {
-		fail("Not yet implemented");
+		Fred.register(foodora);
+		assertTrue(foodora.getUserList().containsValue(Fred));
+		Fred.unregister(foodora);
+		assertTrue(!foodora.getUserList().containsValue(Fred));
 	}
 
 	@Test
 	public void testGetSurname() {
-		fail("Not yet implemented");
+		assertEquals("fredo",Fred.getSurname());
 	}
 
 	@Test
 	public void testSetSurname() {
-		fail("Not yet implemented");
+		Fred.setSurname("fredo2");
+		assertEquals("fredo2",Fred.getSurname());
+		Fred.setSurname("fredo");
+		assertEquals("fredo",Fred.getSurname());
 	}
 
 	@Test
-	public void testIsOnDuty() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testSetOnDuty() {
-		fail("Not yet implemented");
+	public void testOnDuty() {
+		Fred.setOnDuty(true);
+		assertTrue(Fred.isOnDuty());
 	}
 
 	@Test

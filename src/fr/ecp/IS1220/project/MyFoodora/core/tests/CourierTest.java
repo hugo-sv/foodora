08bee0b0b2b659230f8ac17f174d77d5578065df
Hugo Saint-Vignes <hugo.saint-vignes@student.ecp.fr>
2017-04-10 14:44:02 +0200

    Creating test fonctions

---
+++ src/fr/ecp/IS1220/project/MyFoodora/core/tests/CourierTest.java
@@ -0,0 +1,169 @@
+package fr.ecp.IS1220.project.MyFoodora.core.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CourierTest {
+
+	@Test
+	public void testCourierStringStringStringDoubleDoubleStringString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCourierStringStringStringDoubleDouble() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRegister() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUnregister() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSurname() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetSurname() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsOnDuty() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetOnDuty() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPropose() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAccept() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRefuse() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUserStringString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetMyFoodora() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUserStringStringDoubleDouble() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetiD() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsActivated() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetActivated() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetAddressX() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetAddressY() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetAddress() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetAddressX() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetAddressY() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetMoney() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetMoney() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetUsername() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetUsername() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetEmail() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetEmail() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPhoneNumber() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetPhoneNumber() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/fr/ecp/IS1220/project/MyFoodora/core/tests/TimeTest.java
@@ -0,0 +1,14 @@
+package fr.ecp.IS1220.project.MyFoodora.core.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TimeTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}

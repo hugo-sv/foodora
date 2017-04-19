66050b96043f2a121be8fb49805a2b487c5c21fc
Hugo Saint-Vignes <hugo.saint-vignes@student.ecp.fr>
2017-04-19 11:34:09 +0200

    Ajout de presque (a verifier) toute les fonction, le moins dur est fait

--- src/fr/ecp/IS1220/project/MyFoodora/CLUI/Interpreter.java
+++ src/fr/ecp/IS1220/project/MyFoodora/CLUI/Interpreter.java
@@ -38,6 +38,84 @@ public class Interpreter {
 		case "help":
 			this.help();
 			break;
+		case "logout":
+			this.logout(arguments);
+			break;
+		case "registerRestaurant":
+			this.registerRestaurant(arguments);
+			break;
+		case "registerCustomer":
+			this.registerCourier(arguments);
+			break;
+		case "registerCourier":
+			this.registerCourier(arguments);
+			break;
+		case "addDishRestauarantMenu":
+			this.addDishRestauarantMenu(arguments);
+			break;
+		case "createMeal":
+			this.createMeal(arguments);
+			break;
+		case "addDish2Meal":
+			this.addDish2Meal(arguments);
+			break;
+		case "showMeal":
+			this.showMeal(arguments);
+			break;
+		case "saveMeal":
+			this.createMeal(arguments);
+			break;
+		case "setSpecialOffer":
+			this.setSpecialOffer(arguments);
+			break;
+		case "removeFromSpecialOffer":
+			this.removeFromSpecialOffer(arguments);
+			break;
+		case "createOrder":
+			this.createOrder(arguments);
+			break;
+		case "addItem2Order":
+			this.addItem2Order(arguments);
+			break;
+		case "endOrder":
+			this.endOrder(arguments);
+			break;
+		case "onDuty":
+			this.onDuty(arguments);
+			break;
+		case "offDuty":
+			this.offDuty(arguments);
+			break;
+		case "findDeliverer":
+			this.findDeliverer(arguments);
+			break;
+		case "setDeliveryPolicy":
+			this.logout(arguments);
+			break;
+		case "setProfitPolicy":
+			this.setProfitPolicy(arguments);
+			break;
+		case "associateCard":
+			this.associateCard(arguments);
+			break;
+		case "showCourierDeliveries":
+			this.showCourierDeliveries(arguments);
+			break;
+		case "showRestaurantTop":
+			this.showRestaurantTop(arguments);
+			break;
+		case "showCustomers":
+			this.showCustomers(arguments);
+			break;
+		case "showMenuItem":
+			this.showMeal(arguments);
+			break;
+		case "showTotalProfit":
+			this.showTotalProfit();
+			break;
+		case "runTest":
+			this.offDuty(arguments);
+			break;
 		case "suscribe":
 			this.suscribe();
 			break;
@@ -46,15 +124,111 @@ public class Interpreter {
 			break;
 		case "quit":
 			return this.quit();
-		case "showTotalProfit":
-			// un exemple de fonction pas autorisée à tous
-			this.showTotalProfit();
-			break;
 		default:
 			System.out.println("Unknow command, use the help command.");
 		}
 		System.out.println("");
 		return true;
+	}
+
+	private void showCourierDeliveries(String[] arguments) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void showRestaurantTop(String[] arguments) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void showCustomers(String[] arguments) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void associateCard(String[] arguments) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void setProfitPolicy(String[] arguments) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void findDeliverer(String[] arguments) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void offDuty(String[] arguments) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void endOrder(String[] arguments) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void onDuty(String[] arguments) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void addItem2Order(String[] arguments) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void createOrder(String[] arguments) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void removeFromSpecialOffer(String[] arguments) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void setSpecialOffer(String[] arguments) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void showMeal(String[] arguments) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void addDish2Meal(String[] arguments) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void createMeal(String[] arguments) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void registerRestaurant(String[] arguments) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void addDishRestauarantMenu(String[] arguments) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void registerCourier(String[] arguments) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void logout(String[] arguments) {
+		// TODO Auto-generated method stub
+		
 	}
 
 	private void showTotalProfit() {

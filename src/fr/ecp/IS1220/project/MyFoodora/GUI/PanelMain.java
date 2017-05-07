0d7c1b560ed064ed6204b10c7dd28d731b2fc6c2
Hugo Saint-Vignes <hugo.saint-vignes@student.ecp.fr>
2017-05-07 14:43:47 +0200

    Removing GUI

--- src/fr/ecp/IS1220/project/MyFoodora/GUI/PanelMain.java
+++
@@ -1,7 +0,0 @@
-package fr.ecp.IS1220.project.MyFoodora.GUI;
-
-public class PanelMain {
-	public static void main(String[] args) {
-		ActionListenerDemo fenetre = new ActionListenerDemo("My Foofora app");
-	}
-}
